8903598f4d6fea50c454e54b049e9c55bc2632f6
Isaac <isaacochoagarriga@gmail.com>
2026-03-24 17:00:43 +0100

    Commit hanoi

--- docs/extensions/domains/handcrafted_reader.py
+++ docs/extensions/domains/handcrafted_reader.py
@@ -1,7 +1,7 @@
 from docs.extensions.domains import compilation_solving
 from unified_planning.io import PDDLReader
 
-domain = '15-puzzle'
+'''domain = '15-puzzle'
 instance = 'korf1'
 solving = 'fast-downward'
 
@@ -11,4 +11,32 @@ problem_filename = f'{domain}/handcrafted/{instance}.pddl'
 
 problem = reader.parse_problem(domain_filename, problem_filename)
 
-compilation_solving.compile_and_solve(problem, solving, compilation_kinds_to_apply=[])
+compilation_solving.compile_and_solve(problem, solving, compilation_kinds_to_apply=[])'''
+
+from unified_planning.io import PDDLReader
+from unified_planning.shortcuts import OneshotPlanner
+from docs.extensions.domains import compilation_solving
+
+# Paths to your files
+domain_file = "/docs/extensions/domains/hanoi.pddl"
+problem_file = "/docs/extensions/domains/hanoi-3.pddl"
+
+# Read PDDL files
+reader = PDDLReader()
+problem = reader.parse_problem(domain_file, problem_file)
+print(problem.kind)
+
+
+#compilation_solving.compile_and_solve(problem=problem, solver="fast-downward", compilation="ut-integers",timeout=1800)
+
+
+# Solve the problem
+#with OneshotPlanner(name="fast-downward",problem_kind=problem.kind) as planner:
+#    result = planner.solve(problem)
+#
+# Print results
+#if result.plan is not None:
+#    print("Plan found:")
+#    print(result.plan)
+#else:
+#    print("No plan found.")"""
